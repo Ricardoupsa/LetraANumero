f58c304906822349cb8b6a9a39f3d8e36bbc7d5b
Ricardo Gonzales Delgado <a2018118765@estudiantes.upsa.edu.bo>
2020-08-20 12:48:24 -0400

    unitest

---
+++ NumeroaLetra/UnitTestLiteral/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumeroaLetra.Controllers; 
+
+namespace UnitTestLiteral
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            //LiteralController literalController = new LiteralController();
+            //int numero = 60;
+            //string num2Text = "SESENTA";
+            //act
+            //int resultado = literalController.Literalget(numero);
+            //assert
+        }
+    }
+}
